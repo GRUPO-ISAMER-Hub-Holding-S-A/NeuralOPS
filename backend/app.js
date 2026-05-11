2cd9405271a477d1a2807df6282af18d29a233c3
Belen Villar <belu.dvt@hotmail.com>
2026-05-11 01:19:23 -0300

    add calendar

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,23 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import leadRoutes from "./routes/leadRoutes.js";
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
+
 app.use(express.json());
 
 app.use("/api", leadRoutes);
+app.use(limiter);
 
 
 console.log("ENV cargado:", !!process.env.EMAIL_USER);
