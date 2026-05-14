110f94a04858bff30d9dd368e0762aa3d105b6a2
Belen Villar <belu.dvt@hotmail.com>
2026-05-14 00:38:33 -0300

    debug render startup

--- backend/app.js
+++ backend/app.js
@@ -6,36 +6,63 @@ import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+
 import leadRoutes from "./routes/leadRoutes.js";
 
 const app = express();
 
+
+// 🔥 IMPORTANTE RENDER
 app.set("trust proxy", 1);
 
+
+// MIDDLEWARES
 app.use(cors());
 
 app.use(helmet());
 
-
-app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100}));
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+}));
 
 app.use(express.json());
 
+
+// ROUTES
 app.use("/api", leadRoutes);
 
+
+// TEST API
 app.get("/", (req, res) => {
     res.send("NeuralOps API funcionando 🚀");
 });
 
-console.log("ENV cargado:", !!process.env.EMAIL_USER);
 
-// conexión Mongo
+// DEBUG ENV
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("MONGO:", !!process.env.MONGO_URI);
+console.log("JWT:", !!process.env.JWT_SECRET);
+
+
+// MONGO
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("Mongo conectado ✅"))
-  .catch(err => console.log(err));
 
+    .then(() => {
+        console.log("Mongo conectado ✅");
+    })
+
+    .catch((err) => {
+        console.log("❌ ERROR MONGO");
+        console.log(err);
+    });
+
+
+// PORT
 const PORT = process.env.PORT || 4000;
 
+
+// START SERVER
 app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto", PORT);
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
